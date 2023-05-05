3e4d90749045618698eb521924115c4f4502ee1b
NguyenAnhKietUIT <20521498@gm.uit.edu.vn>
2023-05-05 09:32:15 +0700

    Add Forn is named FormHome

--- WeddingManagement/Program.cs
+++ WeddingManagement/Program.cs
@@ -8,12 +8,17 @@ namespace WeddingManagement
 {
     internal static class Program
     {
+        internal static FormHome mainform;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            MessageBoxManager.Yes = "Check bill";
+            MessageBoxManager.No = "Wedding Info";
+            MessageBoxManager.Cancel = "Cancel";
+            MessageBoxManager.Register();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormLogin());
